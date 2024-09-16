ffa3d20b6a311378b9c03041f3ddb713044a9a9c
Thiago Sores Figueiredo <thiagosf.dev@gmail.com>
2024-09-16 10:11:33 -0300

    ci: Update docs workflow

--- packages/docs-v6/.storybook/main.js
+++ packages/docs-v6/.storybook/main.js
@@ -15,7 +15,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === "PRODUCTION") {
-      config.base = "/05-design-system/";
+      config.base = "/ignite-design-system-monorepo/";
     }
 
     return config;
