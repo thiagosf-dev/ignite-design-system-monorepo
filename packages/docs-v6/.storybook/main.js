fa225cb8c654809c5565462b27db444dc9ea8c6e
Thiago Sores Figueiredo <thiagosf.dev@gmail.com>
2024-09-16 09:47:38 -0300

    ci: Update docs workflow

--- packages/docs-v6/.storybook/main.js
+++ packages/docs-v6/.storybook/main.js
@@ -1,13 +1,10 @@
 module.exports = {
-  stories: [
-    "../src/pages/**/*.stories.mdx",
-    "../src/stories/**/*.stories.tsx",
-  ],
+  stories: ["../src/pages/**/*.stories.mdx", "../src/stories/**/*.stories.tsx"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/addon-a11y"
+    "@storybook/addon-a11y",
   ],
   framework: "@storybook/react",
   core: {
@@ -15,5 +12,12 @@ module.exports = {
   },
   features: {
     storyStoreV7: true,
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === "PRODUCTION") {
+      config.base = "/05-design-system/";
+    }
+
+    return config;
   },
 };
